e482f1799fba2a618559a747f4ffdcb5f0e8220f
Romario Campos <romarioa@synnex.com>
2020-10-14 13:54:07 -0300

    Implementando API

---
+++ TreinamentoPrion/backend/Context/Class1.cs
@@ -0,0 +1,20 @@
+﻿using backend.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.Context
+{
+    public class GenericContext : DbContext
+    {
+        public GenericContext() : base("")
+        {
+
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set;}
+    }
+}
---
+++ TreinamentoPrion/backend/Repositories/GenericRepository.cs
@@ -0,0 +1,43 @@
+﻿using backend.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend.Repositories
+{
+    public class GenericRepository<T>: IDisposable where T : class
+    {
+        private GenericContext context;
+
+        public GenericRepository()
+        {
+            context = new GenericContext();
+        }
+
+        public void Add(T entity)
+        {
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            context.Entry(entity).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
