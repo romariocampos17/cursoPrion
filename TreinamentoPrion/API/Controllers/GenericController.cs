4b8582f037c59c0b9a0072398bcdc85c149cc207
Romario Campos <romarioa@synnex.com>
2020-10-26 22:36:11 -0300

    tentando colocar http rest

--- TreinamentoPrion/API/Controllers/GenericController.cs
+++ TreinamentoPrion/API/Controllers/GenericController.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static System.Net.WebRequestMethods;
 
 namespace API.Controllers
 {
+    [Http.RoutePrefix("api")]
     public class GenericController<T> : Controller  where T : class
     {
         protected GenericService<T> services;
@@ -16,26 +18,59 @@ namespace API.Controllers
             services = new Backend.Services.GenericService<T>();
         }
 
+        /// <summary>
+        /// Serviço responsável por atualizar os dados no Application
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        [Http.Route("Add")]
+        [Http.HttpPost]
         public virtual void Add(T entity)
         {
             services.Add(entity);
         }
 
+        /// <summary>
+        /// Serviço responsável por atualizar os dados no Application
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        [Http.Route("Update")]
+        [Http.HttpPost]
         public virtual void Update(T entity)
         {
             services.Update(entity);
         }
 
+        /// <summary>
+        /// Serviço responsável por atualizar os dados no Application
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        [Http.Route("Delete")]
+        [Http.HttpPost]
         public virtual void Delete(T entity)
         {
             services.Delete(entity);
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [Http.Route("{id:int}/FindById")]
+        [Http.HttpGet]
         public virtual T FindById(int id)
         {
             return services.FindById(id);
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param></param>
+        /// <returns></returns>
+        [Http.Route("FindAll")]
+        [Http.HttpGet]
         public virtual List<T> FindAll()
         {
             return services.FindAll();
