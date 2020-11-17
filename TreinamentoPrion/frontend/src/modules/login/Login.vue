d5cf4f0063488148451b66879a087a763ca1ae53
Romario Campos <romarioa@synnex.com>
2020-11-17 09:11:17 -0300

    adicionando state, mutation, sctions para validacao do login

--- TreinamentoPrion/frontend/src/modules/login/Login.vue
+++ TreinamentoPrion/frontend/src/modules/login/Login.vue
@@ -1,36 +1,60 @@
 <template>
-  <v-container id="login" class="">
-    <v-row>
-      <v-col>
-        <v-text-field>
+  <v-container id="login" >
+    <v-form @submit="login">
+      <v-row>
+      <v-col >
+        <v-text-field v-model="payload.login"  label="Login">
+        </v-text-field>
+      </v-col>
+    </v-row>
 
+    <v-row >
+      <v-col >
+        <v-text-field v-model="payload.password" label="Password">
         </v-text-field>
       </v-col>
     </v-row>
 
     <v-row>
       <v-col>
-        <v-text-field>
-        </v-text-field>
+        <v-btn type="submit">Login</v-btn>
       </v-col>
     </v-row>
-
-    <v-row>
-      <v-col>
-        <v-btn>Login</v-btn>
-      </v-col>
-    </v-row>
+    </v-form>
   </v-container>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
+  data() {
+    return {
+      payload: {},
+    };
+  },
+  methods: {
+    ...mapActions([
+      'setUser',
+    ]),
+    login() {
+      if (this.payload.login === '123456' && this.payload.password === '123456') {
+        this.setUser(JSON.stringify(this.payload));
+        console.log('Logou!!!');
+      // else {
+      //     console.log('Acesso Negado')
+      //   }
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
-#login |{
-
+#login {
+  background-color: #42b983;
+  width: 30%;
+  border-radius: 20px;
 }
 
 </style>
--- TreinamentoPrion/frontend/src/store/index.js
+++ TreinamentoPrion/frontend/src/store/index.js
@@ -5,10 +5,20 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    isLogged: false,
+    user: {},
   },
   mutations: {
+    SET_USER(state, payload) {
+      state.user = payload;
+      state.isLogged = true;
+      localStorage.setItem('user', payload);
+    },
   },
   actions: {
+    setUser({ commit }, payload) {
+      commit('SET_USER', payload);
+    },
   },
   modules: {
   },
